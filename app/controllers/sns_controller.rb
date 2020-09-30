4791e498b530b8a3d1e5c742dad58c3b536f35f7
tshuhei <tsushuhei@gmail.com>
2020-09-30 21:06:21 +0900

    homeControllerとsnsControllerを作成し、それぞれのルーティングを設定した。アクションも宣言だけした。

---
+++ app/controllers/sns_controller.rb
@@ -0,0 +1,27 @@
+class SnsController < ApplicationController
+
+    def twitter
+    end
+
+    def twitterSearch
+    end
+
+    def linkedin
+    end
+
+    def linkedinSearch
+    end
+
+    def tiktok
+    end
+
+    def tiktokSearch
+    end
+
+    def instagram
+    end
+
+    def instagramSearch
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root "home#index"
+
+  get "/twitter", to: "sns#twitter"
+  post "/twitter", to: "sns#twitterSearch"
+  
+  get "/linkedin", to: "sns#linkedin"
+  post "/linkedin", to: "sns#linkedinSearch"
+
+  get "/tiktok", to: "sns#tiktok"
+  post "/tiktok", to: "sns#tiktokSearch"
+
+  get "/instagram", to: "sns#instagram"
+  post "/instagram", to: "sns#instagramSearch"
 end
