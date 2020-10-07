eed7f285d94a965eeab692099ef12c7d70e9290b
turituri08 <71017718+turituri08@users.noreply.github.com>
2020-10-07 18:55:23 +0900

    top page completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,19 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+require ('test.js')
+
+//= require bootstrap
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,3 +22,7 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
